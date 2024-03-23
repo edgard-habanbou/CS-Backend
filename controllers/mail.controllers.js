74f962bed132eb9578cb1edf92d1c95c34027c4d
edgard <edgard.habanbou@hotmail.com>
2024-03-23 10:53:59 +0200

    Added Mail comtroller

---
+++ controllers/mail.controllers.js
@@ -0,0 +1,33 @@
+const nodemailer = require("nodemailer");
+
+const sendMail = async (email, subject, message, res) => {
+  if (!email || !subject || !message) {
+    res.status(400).send({ message: "all fields are required" });
+    return;
+  }
+  const EmailAddress = process.env.EMAIL_ADDRESS;
+  const EmailPassword = process.env.EMAIL_PASSWORD;
+  try {
+    const transporter = nodemailer.createTransport({
+      service: "outlook",
+      auth: {
+        user: EmailAddress,
+        pass: EmailPassword,
+      },
+    });
+    const mailOptions = {
+      from: `"CarServify" <${EmailAddress}>`,
+      to: email,
+      subject: subject,
+      text: message,
+    };
+    await transporter.sendMail(mailOptions);
+    res.status(200).send({ message: "email sent successfully" });
+  } catch (err) {
+    res.status(500).send({ message: "something went wrong" });
+  }
+};
+
+module.exports = {
+  sendMail,
+};
